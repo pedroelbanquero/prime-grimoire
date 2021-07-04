b0317b9345d94133882f1cc34bdd5f50a3019a0a
Pedro el banquero <60758685+pedroelbanquero@users.noreply.github.com>
2021-07-04 10:53:13 -0500

    Update nsif.hs

--- nsif.hs
+++ nsif.hs
@@ -25,98 +25,13 @@ import Codec.Crypto.RSA.Pure
 
 
 
-prim n = read ((splitOn " " $ show (P.nextPrime n)) !! 1)::Integer
-
-
-primb n = read ((splitOn " " $ show (P.precPrime n)) !! 1)::Integer
-
-
-ncr n b e t=  head $ filter (\(r,w)-> w/=1 && w/=r ) $ map (\x-> (n, (gcd n ((powMod x (n^2-x^2) n) - x ))) )  $ map (\x-> (x+3))  [0..t]-- [(e^b..e^b+t] 
-
-
-loadprimes = do 
-	a<- readFile "../../../safes25.txt"
-	let b = splitOn "\n" a
-	let c = init $ map (\x-> read x::Integer) b
-
-	return c
-
-primc n l = map (\x-> read ((splitOn " " $ show (P.nextPrime x)) !! 1)::Integer) [1..l]
-
-powRoot n = minimum $ intpowroot (n)
-
-bestroot n = minimum $ nub $ sort $ filter (\(r,e,w)-> e/=n ) $  map (\x-> (n-((integerRoot x n))^x,(integerRoot x n),x) ) [1..1000]
-
-powDiv n o
-	| rest == 0 || rest == 1 || rest == 2 = (o++[(b,e)])
-	| otherwise = powDiv rest (o++[(b,e)])
-	where
-	(rest,b,e) = bestroot n
-		
-
-intpowroot n =  filter (\(r,f,g)-> (f^g)<=n) $ concat $  map (\x-> map (\y-> (n-(x^y),x,y) ) [2..512]) [2..512]
-
-bestpow n =  head $ filter (<n) $  map (\x-> map (\y-> (y^x) ) [1..1000]) [1..15]
-
-supcar primespair o
-	| lp == 1  = out
-	| otherwise = supcar to out
-	where
-	lp = length primespair
-	to = tail primespair
-	out = lcm (o) (head primespair)
-
-nsifc n tries
-	| out2 /= 1 && out2 /= n = (div n out2,out2) 
-	| out /=1 && out /= n = (div n out,out)
-	| out3 /= 1 && out3 /= n = (div n out3,out3) 
-	| out4 /=1 && out4 /= n = (div n out4,out4)
-	| out5 /= 1 && out5 /= n = (div n out5,out5) 
-	| out6 /=1 && out6 /= n = (div n out6,out6)
-	| out7 /= 1 && out7 /= n = (div n out7,out7) 
-	| out8 /=1 && out8 /= n = (div n out8,out8)
-
-	| otherwise = (0,0)
-	where
- 	base = 2
-		
-	--(nearsquare) = 2^(logBase 2 n)
-	primesc = nub $ sort $ map prim [1..n]	
-	out =  head $ reverse ([1] ++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) (x*(n + (n - x))) x)) $ reverse $ [2^base..2^base+tries]) )
-	out2 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^2-(x)^2) x)) [2^base..2^base+tries]))
-
-	out3 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3+(x)^3) x)) [3^base..3^base+tries]))
-	out4 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3-(x)^3) x)) [3^base..3^base+tries] ))
-	out5 =  head $ reverse ([1] ++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5+(x)^5) x)) [5^base..5^base+tries]) )
-	out6 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5-(x)^5) x)) [5^base..5^base+tries]))
-	out7 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7+(x)^7) x)) [7^base..7^base+tries]))
-	out8 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7-(x)^7) x)) [7^base..7^base+tries] ))
-
-
-
-
-sp s l = nub $ sort $  concat $ map (\x-> map (\y-> x*y) (map (\e-> prim (e*2) ) [(s)..(s)+l]) ) (map (\t-> prim (t*3)) [0,(s)..(s)+l])
-
-
 
 
 ex2 = 1826379812379156297616109238798712634987623891298419
 
 ex = 13 
 
-tryperiod2 n period m = (powMod m (ex*modular_inverse ex period) n) - (m)
-
-
 tryperiod n period _ = (powMod 2 (ex * modular_inverse ex period - 1) n) - 1
-{--
--- | Cypher 'm', and tries to uncypher using 'period' as the subgroup order
-tryperiod n period m = 
-   m == powMod c xe n   -- uncypher c, and test if equal to original message
-   where
-   c  = powMod m ex n   -- cypher m
-   -- 'xe' would be the privKey, inverse of 'ex', if 'period' was a subgroup order
-   xe = modular_inverse ex period
---}   
 
 primetosquare :: Integer -> [Integer]
 {- | Search for squares 'o2' and check if subtracting (n - 1) is prime.  -}
@@ -133,42 +48,13 @@ primetosquare n = candidates ini (ini^2)
       x  = o2 - n + 1   -- (n - 1 + x) must be a perfect square 
 
 
-intPowBaseExp n= head $  map (\[h,j,k]-> [k,h]) $ filter (\[e,r,u]-> r=="0") $ map (\([a,b],c)-> [a,b,show c]) $ tail $  map (\x-> (splitOn "." $ show $ (logBase x n),x)) [2..3000]
-
 
 
 nsif n tries distance
 	| d /=1 && d /= n = (div n d,d,divcar)
 	| otherwise = (0,0,0)
 	where
-	modin = head $ map fst $ powDiv n [] 
 	out2 = reverse ((1,1): take 1 (dropWhile (\(r,u)-> r==1 && r /= n ) $ map (\x-> (gcd (n) ((tryperiod ((n)) ((n)^2-x^2) x)  ),x)) $ [distance..distance+tries]))
 	d = fst $ head out2
 	divcar = snd $ head out2
 
-
---
---
-main = do  
-    args <- getArgs                  -- IO [String]
-    progName <- getProgName          -- IO String
-    print args
-    let (n : st :  m : _) = args
-    -- let n = args !! 0
-    -- let st = args !! 1
-    -- let e = args !! 2
-    -- let m = args !! 3
-
-    let (factorA,factorB) = nsifc (read n::Integer) (read m::Integer) 
-    
-    putStrLn "Public Key" 
-    
-    print $ (read n::Integer)
-
-    putStrLn $ "Factors"
-
-    print $ "P " ++ show factorB
-
-    print $ "Q " ++ show factorA
-    
-    putStrLn $ "Prime grimoire spells  v0.1"
